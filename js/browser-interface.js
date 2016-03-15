cf3758400135091b8d186f28b91bd24e23f3e893
Michelle Brecunier and Schuyler Klaassen <michelle.brecunier@gmail.com>
2016-03-15 14:41:54 -0700

    show message and picture when alarm goes off

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,9 +1,25 @@
 $(document).ready(function(){
-  $('#currentTime').text(moment());
+  var currentTime = moment().format('HH:mm');
+  var alarmTime;
+  function update() {
+    $('#currentTime').text(moment().format('HH:mm:ss'));
+    currentTime = moment().format('HH:mm');
+    if(alarmTime === currentTime) {
+      console.log("PARTY TIIIIIIIIIME");
+      $('#ptext').append("<p>PARTY TIIIIIIIIIME</p>");
+      $('#ppic').append("<img src='img/wk.jpg'>");
+
+    } else {
+      console.log("Not yet ppppppartytime");
+      console.log(currentTime);
+    }
+  }
+  setInterval(update, 1000);
 
   $('#alarmForm').submit(function(event) {
     event.preventDefault();
-    var alarmTime = $("#alarmTime").val();
+    alarmTime = $("#alarmTime").val();
     console.log(alarmTime);
-  })
+  });
+
 });
